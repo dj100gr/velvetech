0ac1b9511573f5fb785eeabdba2bec25bfd2d5c2
Юрий Колыванов <dj100gr@gmail.com>
2021-04-19 11:48:14 +0700

    create project

---
+++ velvetech/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace velvetech.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public int Pol { get; set; }
+        public string Fam { get; set; }
+        public string Nam { get; set; }
+        public string Oth { get; set; }
+        public string Alias { get; set; }
+    }
+}
---
+++ velvetech/Repositories/TzBdContex.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using velvetech.Models;
+
+namespace velvetech.Repositories
+{
+    public class TzBdContex : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\velvetech.db");
+    }
+}
